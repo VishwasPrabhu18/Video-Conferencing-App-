e33c5b5607f4ef4d181f11666304b92ff86bc301
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-01 13:13:07 +0530

    home card listed

---
+++ components/MeetingTypeList.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from 'react';
+import HomeCard from './HomeCard';
+import { useRouter } from 'next/navigation';
+
+const MeetingTypeList = () => {
+
+  const router = useRouter();
+
+  const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>();
+
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+      <HomeCard
+        imgSrc="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState('isJoiningMeeting')}
+        className="bg-orange-1"
+      />
+
+      <HomeCard
+        imgSrc="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setMeetingState('isScheduleMeeting')}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        imgSrc="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings"
+        handleClick={() => router.push('/recordings')}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        imgSrc="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link or ID"
+        handleClick={() => setMeetingState('isJoiningMeeting')}
+        className="bg-yellow-1"
+      />
+    </section>
+  )
+}
+
+export default MeetingTypeList
