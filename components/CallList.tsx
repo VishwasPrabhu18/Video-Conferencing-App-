fa1699102a19922b16661236657bf9063f6cd15e
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-03 13:07:58 +0530

    REcorded Meeting is done

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -2,12 +2,13 @@
 
 "use client";
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGetCalls } from '@/hooks/useGetCalls'
 import { useRouter } from 'next/navigation';
 import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import MeetingCard from './MeetingCard';
 import { Loader } from 'lucide-react';
+import { toast } from './ui/use-toast';
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
 
@@ -35,10 +36,29 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
     }
   };
 
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      try {
+        const callData = await Promise.all(callRecordings.map((meeting) => meeting.queryRecordings()));
+  
+        const recordings = callData
+          .filter(call => call.recordings.length > 0)
+          .flatMap(call => call.recordings);
+        
+        setRecordings(recordings);
+      } catch (err) {
+        console.log(err);
+        toast({ title: "Try Again Later" });
+      }
+    }
+
+    if (type === "recordings") fetchRecordings();
+  }, [type, callRecordings]);
+
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage();
 
-  if(isLoading) return <Loader />
+  if (isLoading) return <Loader />
 
   return (
     <div className='grid grid-cols-1 gap-5 xl:grid-cols-2'>
@@ -52,12 +72,12 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
               (meeting as CallRecording).filename?.substring(0, 20) ||
               'No Description'
             }
-            date={(meeting as Call).state.startsAt?.toLocaleString() || meeting.start_time.toLocaleString()}
-            isPreviousMeeting={type==="ended"}
-            buttonIcon1={type==="recordings" ? "/icons/play.svg" : undefined}
-            buttonText={type==="recordings" ? "Play": " Start"}
-            link={type ==="recordings" ? meeting.url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`}
-            handleClick={type==="recordings" ? () => router.push(`${meeting.url}`) : () => router.push(`/meeting/${meeting.id}`)}
+            date={meeting.state?.startsAt?.toLocaleString() || meeting.start_time.toLocaleString()}
+            isPreviousMeeting={type === "ended"}
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            buttonText={type === "recordings" ? "Play" : " Start"}
+            link={type === "recordings" ? meeting.url : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`}
+            handleClick={type === "recordings" ? () => router.push(`${meeting.url}`) : () => router.push(`/meeting/${meeting.id}`)}
           />
         )) : (
           <h1>{noCallsMessage}</h1>
